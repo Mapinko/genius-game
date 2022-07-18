c6d0df9adf8c492d4fdeb9e3b025f97626e28c51
Mari Spinola <mapincosf@gmail.com>
2022-07-18 19:52:58 -0300

    fix: h1 and h2 default style

--- src/style/global.js
+++ src/style/global.js
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-
-* {
+@import url('https://fonts.googleapis.com/css2?family=MuseoModerno&display=swap');/*h1 font*/
+@import url('https://fonts.googleapis.com/css2?family=Jura:wght@600&display=swap'); /*h2 font*/
+@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap'); /*h3 font*/
+	* {
 		box-sizing: border-box;
 	}
      body{
@@ -14,6 +16,16 @@ export const GlobalStyle = createGlobalStyle`
 	}
 	ul, ol, li{
 		list-style:none;
+	}
+	h1{
+		color: #F06060;
+		font-family: 'MuseoModerno', cursive;
+		font-size: 5rem;
+		text-transform: uppercase;
+	}
+	h2 {
+		font-family: "Kumbh Sans", sans-serif;
+		font-size: 1.5rem;	
 	}
 
 `;
