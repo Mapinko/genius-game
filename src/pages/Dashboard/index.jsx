a5822d9d6c54e6de3ae4fab142adb3bd9027a8ff
Mari Spinola <mapincosf@gmail.com>
2022-07-18 23:29:25 -0300

    feat: instructions route

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,20 @@
+import { useHistory } from "react-router-dom";
 import { ButtonStyle } from "../../components/Button/style";
 import NavBarDesktop from "../../components/NavBarDesktop";
 
 const Dashboard = () => {
+	const history = useHistory();
+
+	const handlePage = (route) => {
+		history.push(route);
+	};
+
 	return (
 		<>
 			<NavBarDesktop>
-				<ButtonStyle>Regras</ButtonStyle>
+				<ButtonStyle onClick={() => handlePage("/")}>
+					Regras
+				</ButtonStyle>
 				<ButtonStyle>Login</ButtonStyle>
 				<ButtonStyle>Criar Conta</ButtonStyle>
 			</NavBarDesktop>
